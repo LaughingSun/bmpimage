db03d07ebff4c71da966b9d0c18ccce737a7a374
kolesov93 <kolesov93@gmail.com>
2013-12-18 22:15:03 +0300

    Put all stuff into namespace

--- bmpimage.h
+++ bmpimage.h
@@ -4,18 +4,40 @@
 using std::string;
 using std::vector;
 
+namespace NImage {
+
 class BMPImage {
-private:
+public:
     typedef unsigned char byte;
     typedef vector<byte> bytes;
 private:
     bytes fileHeader;
+    bytes pallete;
     bytes imageHeader;
     bytes imageData;
     
     int width, height;
     int bitsPerPixel;
+
 public:
     BMPImage(const string& filename);
     BMPImage(const bytes& data);
+
+    
 };
+
+BMPImage::bytes read(const string& filename) {  
+
+}
+
+
+
+string toHex(byte value) {
+    string 
+}
+
+void outputHex(const bytes& data) {
+    
+}
+
+}
